392429e5e8a4b8f92a22f52577af194def54c75b
DentonWarnock <dentonwarnock@gmail.com>
2019-12-04 12:45:49 -0800

    dashboard tests added and passed.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
-// Test away
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+
+test("display dashboard component snapshot", () => {
+  const element = render(<Dashboard />);
+  expect(element.asFragment()).toMatchSnapshot();
+});
+
+test("dashboard renders and displays the words open and gate", () => {
+  const wrapper = render(<Dashboard />);
+  const open = wrapper.getByText(/open/i);
+  const gate = wrapper.getByText(/unlocked/i);
+  expect(open).toBeInTheDocument();
+  expect(open).toBeVisible();
+  expect(gate).toBeInTheDocument();
+  expect(gate).toBeVisible();
+});
