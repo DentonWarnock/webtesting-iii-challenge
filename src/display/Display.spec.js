e57030655412078ef0debf61ba9acaac5d8d6dc9
DentonWarnock <dentonwarnock@gmail.com>
2019-12-04 11:39:10 -0800

    First display tests passing.

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
-// Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Display from "./Display";
+
+test("Display component snapshot", () => {
+  const element = rtl.render(<Display />);
+  expect(element.asFragment()).toMatchSnapshot();
+});
+
+test("display renders and displays open", () => {
+  const wrapper = rtl.render(<Display />);
+  const element = wrapper.getByText(/open/i);
+  expect(element).toBeInTheDocument();
+  expect(element).toBeVisible();
+});
